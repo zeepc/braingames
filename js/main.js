f2d9ad681510d86b7a67e46473c12464bf0999f3
zeepc <zperezcanals@gmail.com>
2018-01-22 20:18:13 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,33 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// store card types
+var cards = ['queen','queen','king','king'];
+// will show cards selected by player
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+// cards currently selected by user
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+// Are selected cards a match?
+if (cardsInPlay.length === 2) {
+// Are the cards selected a match?
+
+  if  (cardsInPlay[0] === cardsInPlay[2]) {
+
+		alert("You found a match!");
+	}
+else {
+
+ alert("Sorry, try again!"); 
+
+	}
+
+}
+
+/*
+
+console.log("User flipped " + cardsInPlay);
+*/
