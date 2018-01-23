6b41dc47b6d854a9b0953af56f175df13f9e8c99
zeepc <zperezcanals@gmail.com>
2018-01-23 03:36:01 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// store card types
+// Store card types
 var cards = [
 {
 rank: 'queen',
@@ -28,8 +28,13 @@ cardImage: 'images/queen-of-diamonds.png'
 // Will show cards selected by player
 var cardsInPlay = [];
 
+
+
 // Function to check for matching cards
 var checkForMatch = function() {
+//this.setAttribute('src',cards[cardId].cardImage);
+//the setAttribute does NOT go above. cardId var declared
+// in flipCard function
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
@@ -39,17 +44,30 @@ var checkForMatch = function() {
 
 // Flipcards
 
-var flipCard = function(cardID) {
-	console.log('User flipped ' + cards[cardID].rank + '!')
-	cardsInPlay.push(cards[cardID].rank);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	console.log('User flipped ' + cards[cardId].rank + '!')
+	this.setAttribute('src',cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
-	console.log(cards[cardID].cardImage);
-	console.log(cards[cardID].suit);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src','images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
+
+
 
 
 
