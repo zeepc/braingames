a22c2bf8c76a17faff03035e7e0bd4200d779920
zeepc <zperezcanals@gmail.com>
2018-01-23 00:06:58 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,45 +1,56 @@
 // store card types
-var cards = ['queen','queen','king','king'];
-// will show cards selected by player
+var cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: 'images/queen-of-hearts.png'
+},
+
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: 'images/queen-of-diamonds.png'
+},
+
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: 'images/king-of-hearts.png'
+},
+
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: 'images/queen-of-diamonds.png'
+}
+
+];
+// Will show cards selected by player
 var cardsInPlay = [];
 
-
-// function to check for matching cards
+// Function to check for matching cards
 var checkForMatch = function() {
-if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
-} else {
-console.log("Sorry, try again.");
-}
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
 }
 
-// Function to flip cards
+// Flipcards
 
 var flipCard = function(cardID) {
+	console.log('User flipped ' + cards[cardID].rank + '!')
+	cardsInPlay.push(cards[cardID].rank);
+	checkForMatch();
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 
-// How many  cards selected
-if (cardsInPlay.length === 2) {
-// Are the cards selected a match
-
-  if  (cardsInPlay[0] === cardsInPlay[2]) {
-
-		alert('You found a match!');
-	}
-else {
-
- alert('Sorry, try again!'); 
-
-	} 
-
-}
-
-console.log('User flipped ' + cards[cardID] + '!')
-cardsInPlay.push(cards[cardID]);
-checkForMatch();
 }
 
 flipCard(0);
 flipCard(2);
+
 
 
 
